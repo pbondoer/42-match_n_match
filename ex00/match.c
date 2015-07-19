61ba1d8c245e754929cb8913bbb1188075d51912
Pierre BONDOERFFER <pbondoer@student.42.fr>
2015-07-19 23:27:31 +0200

    match

---
+++ ex00/match.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   match.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/07/19 23:05:09 by pbondoer          #+#    #+#             */
+/*   Updated: 2015/07/19 23:27:16 by pbondoer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i))
+		i++;
+	return (i);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (!(*s1 == '\0' || *s2 == '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+int		match(char *s1, char *s2)
+{
+	if (ft_strcmp(s2, "*") == 0 || ft_strcmp(s1, s2) == 0)
+		return (1);
+	if (ft_strlen(s1) == 0)
+		return (0);
+	if (s1[0] == s2[0])
+		return (match(s1 + 1, s2 + 1));
+	if (s2[0] == '*')
+		return (match(s1 + 1, s2) || match(s1, s2 + 1));
+	return (0);
+}
