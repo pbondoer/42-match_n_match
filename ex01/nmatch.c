91a4b4b261c76413102e87a9d3835d5d62ab4c11
Pierre BONDOERFFER <pbondoer@student.42.fr>
2015-07-19 23:31:33 +0200

    norminette

--- ex01/nmatch.c
+++ ex01/nmatch.c
@@ -6,7 +6,7 @@
 /*   By: pbondoer <pbondoer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/19 23:05:09 by pbondoer          #+#    #+#             */
-/*   Updated: 2015/07/19 23:30:44 by pbondoer         ###   ########.fr       */
+/*   Updated: 2015/07/19 23:31:21 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ int		nmatch(char *s1, char *s2)
 	if (ft_strlen(s1) == 0)
 		return (0);
 	if (s1[0] == s2[0])
-		return 1 + (nmatch(s1 + 1, s2 + 1));
+		return (1 + nmatch(s1 + 1, s2 + 1));
 	if (s2[0] == '*')
-		return 1 + (nmatch(s1 + 1, s2) || nmatch(s1, s2 + 1));
+		return (1 + (nmatch(s1 + 1, s2) || nmatch(s1, s2 + 1)));
 	return (0);
 }
